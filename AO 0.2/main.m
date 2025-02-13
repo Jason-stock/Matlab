06550f94dcf7f83828d1883348aacd8137719c96
Jason <jasonlin2626@gmail.com>
2025-02-14 01:19:31 +0800

    V3

--- AO 0.2/main.m
+++ AO 0.2/main.m
@@ -22,35 +22,39 @@
 clear all 
 clc
 
-Solution_no=20;  
-F_name='F1';    
+Solution_no=20;     
 M_Iter=1000;    
 
+numbers = 13;
+
+for i = 1 : numbers
+    [LB,UB,Dim,F_obj]=Get_F("F"+num2str(i)); 
+    [Best_FF,Best_P,conv]=AO(Solution_no,M_Iter,LB,UB,Dim,F_obj);  
 
 
-[LB,UB,Dim,F_obj]=Get_F(F_name); 
-[Best_FF,Best_P,conv]=AO(Solution_no,M_Iter,LB,UB,Dim,F_obj);  
-
-figure('Position',[454   445   694   297]);
-subplot(1,2,1);
-func_plot(F_name);
-title('Parameter space')
-xlabel('x_1');
-ylabel('x_2');
-zlabel([F_name,'( x_1 , x_2 )'])
+    figure('Position',[454   445   694   297]);
+    subplot(1,2,1);
+    func_plot("F"+num2str(i));
+    title('Parameter space')
+    xlabel('x_1');
+    ylabel('x_2');
+    zlabel(["F"+num2str(i),'( x_1 , x_2 )'])
 
 
-subplot(1,2,2);
-semilogy(conv,'Color','r','LineWidth',2)
-title('Convergence curve')
-xlabel('Iteration#');
-ylabel('Best fitness function');
-axis tight
-legend('Aquila (AO)')
+    subplot(1,2,2);
+    semilogy(conv,'Color','r','LineWidth',2)
+    title('Convergence curve')
+    xlabel('Iteration#');
+    ylabel('Best fitness function');
+    axis tight
+    legend('Aquila (AO)')
 
 
-display(['The best-obtained solution by AO is : ', num2str(Best_P)]);
-display(['The best optimal values of the objective funciton found by AO is : ', num2str(Best_FF)]);
+    display(['The best-obtained solution by AO is : ', num2str(Best_P)]);
+    display(['The best optimal values of the objective funciton found by AO is : ', num2str(Best_FF)]);
+
+end
+
 
         
 
